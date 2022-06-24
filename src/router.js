ea43a6015c9dac524bfbdb6b4d88c8d656ae481c
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-24 02:29:24 -0300

    feat: :sparkles: add GET </user/:id> route
    calls <validateToken> middleware and <getUserById> controller

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ const middlewaresIndex = require('./middlewares/middlewaresIndex');
 router.post('/login', middlewaresIndex.validateLogin, userController.getNewToken);
 
 router.get('/user', middlewaresIndex.validateToken, userController.getAllUsers);
+router.get('/user/:id', middlewaresIndex.validateToken, userController.getUserById);
 router.post('/user', middlewaresIndex.validateNewUser, userController.addUser);
 
 module.exports = router;
