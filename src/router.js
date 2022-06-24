ace32266f2fbe0b94a03ca065ca67f4f37fcc91f
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-24 03:13:33 -0300

    feat: :sparkles: Add POST </categories> route
    Calls <validateToken> and <validateNewCategory> middlewares and <addCategory> Controller

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const userController = require('./controllers/userController');
+const categoryController = require('./controllers/categoryController.js');
 const middlewaresIndex = require('./middlewares/middlewaresIndex');
 
 router.post('/login', middlewaresIndex.validateLogin, userController.getNewToken);
@@ -8,5 +9,13 @@ router.post('/login', middlewaresIndex.validateLogin, userController.getNewToken
 router.get('/user', middlewaresIndex.validateToken, userController.getAllUsers);
 router.get('/user/:id', middlewaresIndex.validateToken, userController.getUserById);
 router.post('/user', middlewaresIndex.validateNewUser, userController.addUser);
+
+// CATEGORIES routes
+router.post(
+  '/categories',
+  middlewaresIndex.validateToken,
+  middlewaresIndex.validateNewCategory,
+  categoryController.addCategory,
+);
 
 module.exports = router;
