dd3cbde66dc963098478a5654cbd42d25d88eb1b
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-24 20:35:12 -0300

    style: :art: Spacings in <blogPosts> at database model

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -5,7 +5,7 @@ const BlogPost = (sequelize, DataTypes) => {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
-    userId: {type: DataTypes.INTEGER, foreignKey: true},
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
     published: DataTypes.DATE,
     updated: DataTypes.DATE,
   });
