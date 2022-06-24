2b95a4e6bea6b0516be095f12f0f623d8e057131
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-24 20:31:59 -0300

    fix: :truck: Changes to <postCategory> at database model
    Changing file and function name

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory", {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  });
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
