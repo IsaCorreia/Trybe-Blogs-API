274ecc5751b0c7ac44e4c998ae61c5a8f679f901
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-24 03:33:30 -0300

    style: :art: Changes to <router>
    Line breaks and spacings

--- src/router.js
+++ src/router.js
@@ -4,10 +4,18 @@ const userController = require('./controllers/userController');
 const categoryController = require('./controllers/categoryController.js');
 const middlewaresIndex = require('./middlewares/middlewaresIndex');
 
-router.post('/login', middlewaresIndex.validateLogin, userController.getNewToken);
+router.post(
+  '/login',
+  middlewaresIndex.validateLogin,
+  userController.getNewToken,
+);
 
 router.get('/user', middlewaresIndex.validateToken, userController.getAllUsers);
-router.get('/user/:id', middlewaresIndex.validateToken, userController.getUserById);
+router.get(
+  '/user/:id',
+  middlewaresIndex.validateToken,
+  userController.getUserById,
+);
 router.post('/user', middlewaresIndex.validateNewUser, userController.addUser);
 
 // CATEGORIES routes
