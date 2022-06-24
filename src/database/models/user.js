61aaaed7fb526b85b7df2d0be07acc5149be5ec1
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-24 15:39:56 -0300

    feat: :sparkles: Add association to <user> at Model layer
    Associates <userId> to <blogPost> model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,11 +1,15 @@
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
-    id: { type: DataTypes.INTEGER, primaryKey: true },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
-  });
+  } );
+  
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: "userId", as: "userId" });
+  };
   return User;
 };
 
