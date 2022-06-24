cf66bc27e608f6d42e7295d05963544a7d89c65d
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-24 02:52:44 -0300

    fix: :truck: Changes to <category> at database model
    Correcting file name and adding id property to model.

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const Categories = (sequelize, DataTypes) => {
-  const Categories = sequelize.define("Categories", {
+  const Categories = sequelize.define( "Category", {
+    id: {type: DataTypes.INTEGER, primaryKey: true},
     name: DataTypes.STRING,
   });
   return Categories;
