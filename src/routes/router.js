b929656222ae25a33c48cf13373f8eacb1b7d777
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-25 00:50:41 -0300

    feat: :sparkles: Add </post> route group to Router

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/login', require('./loginRoutes'));
 router.use('/user', require('./userRoutes'));
 router.use('/categories', require('./categoriesRoutes'));
+router.use('/post', require('./postRoutes.js'));
 
 module.exports = router;
