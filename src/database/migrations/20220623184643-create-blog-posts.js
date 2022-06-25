36bb06993c346fc3ba2ccc21bf9216176398e1f8
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-25 01:10:38 -0300

    fix: :pencil2: Readding <createdAt and updatedAt> fields to <create-blog-posts> migration
    Lines got deleted accidentally

--- src/database/migrations/20220623184643-create-blog-posts.js
+++ src/database/migrations/20220623184643-create-blog-posts.js
@@ -29,6 +29,16 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
