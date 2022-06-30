55ad8905f32d19f4720694e73462bf8c867945fe
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-29 22:15:41 -0300

    feat: :sparkles: Finish <findAll> at postService
    Correcting association and attribute filtering to <findAll>

--- src/services/postService.js
+++ src/services/postService.js
@@ -3,18 +3,19 @@ const { BlogPost, User, Category } = require('../database/models');
 const getAllPosts = async () => {
   const posts = await BlogPost.findAll({
     include: [
-      { model: User, as: 'user' },
-      { model: Category, as: 'categoryId' },
+      {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'displayName', 'email', 'image'],
+      },
+      {
+        model: Category,
+        as: 'categories',
+        through: { attributes: [] },
+      },
     ],
   });
-  const result = posts.map(({ dataValues: postData }) => {
-    const { id, displayName, email, image } = postData.user.dataValues;
-    return {
-      ...postData,
-      user: { id, displayName, email, image },
-    };
-  });
-  return result;
+  return posts;
 };
 
 const getPostById = async ({ id: postId }) => {
